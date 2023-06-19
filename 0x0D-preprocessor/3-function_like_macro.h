4301d098933a5f57ead1ee344e514240e75680f0
root <yssfansri@gmail.com>
2023-06-19 03:51:25 -0700

    k

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,8 +1,8 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION _LIKE_MACRO_H
 
-#define ABS(x) (if(x>0 ? x:-x))
+#define ABS(x) (if((x)>0 ? (x):(-x)))
 
-#endif /_FUNCTIO_LIKE_MACRO_H  */
+#endif /* _FUNCTIO_LIKE_MACRO_H  */
 
 
